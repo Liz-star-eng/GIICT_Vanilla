5fd46de17143a2ba0ea043fb78b375273fe3c637
Liz-star-eng <lisablay27@gmail.com>
2022-11-28 18:34:57 -0500

    Mins message page added, index page modified added rea more

---
+++ index.js
@@ -0,0 +1,15 @@
+const dots = document.getElementById("dots");
+const moreText = document.getElementById("cont");
+const btnText = document.getElementById("readMore");
+
+function Read() {
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
